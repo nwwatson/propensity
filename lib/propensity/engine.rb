4c6d46762fd197a561ec4d2b5cd6fb34380d7306
Nicholas Watson <nwwatson@gmail.com>
2012-09-10 23:48:12 -0400

    added initializer

--- lib/propensity/engine.rb
+++ lib/propensity/engine.rb
@@ -1,5 +1,9 @@
 module Propensity
   class Engine < ::Rails::Engine
     isolate_namespace Propensity
+
+    initializer "propensity.load_preferences", :before => "application.environment" do
+      ::ActiveRecord::Base.send :include, Propensity::Preferences::Preferable
+    end
   end
 end
