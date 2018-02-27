77c16c11cfcc69e30c1daefce7e3ffdf84c916ec
Nicholas Watson <nicholas.w.watson@me.com>
2018-02-27 10:44:03 -0500

    removed rounding up

--- app/models/propensity/preferences/preferable.rb
+++ app/models/propensity/preferences/preferable.rb
@@ -113,7 +113,7 @@ module Propensity
         when :password
           value.to_s
         when :decimal
-          BigDecimal.new(value.to_s).round(2, BigDecimal::ROUND_HALF_UP)
+          BigDecimal.new(value.to_s)
         when :integer
           value.to_i
         when :boolean
